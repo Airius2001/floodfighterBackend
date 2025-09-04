3247242894c636ee85ead64328d58ddcb4a2afa8
Spida <1404994250@qq.com.com>
2025-09-04 20:58:30 +1000

    Add another ts file

--- api/[[...slug]].ts
+++ api/[[...slug]].ts
@@ -1,3 +1,4 @@
+// api/[[...slug]].ts
 export const config = { runtime: 'nodejs22.x' }; // 或 'nodejs20.x'
 
 import 'reflect-metadata';
@@ -5,7 +6,6 @@ import serverless from 'serverless-http';
 import express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from '../src/app.module';
 
 let cached: any;
 
@@ -13,17 +13,22 @@ async function bootstrap() {
   const expressApp = express();
   const adapter = new ExpressAdapter(expressApp);
 
-  const app = await NestFactory.create(AppModule, adapter, { logger: ['error','warn','log'] });
+
+  const { AppModule } = await import('../dist/src/app.module.js');
+
+  const app = await NestFactory.create(AppModule, adapter, {
+    logger: ['error', 'warn', 'log'],
+  });
 
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://floodfighter.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
-  await app.init();           
-  return serverless(expressApp);
+  await app.init();               
+  return serverless(expressApp); 
 }
 
 export default async function handler(req: any, res: any) {
